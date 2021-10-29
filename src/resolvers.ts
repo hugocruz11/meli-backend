b93672c0cc84d12ecaa4020f3cf53eda88ee5151
hcruzmaking <hcruz@makingsense.com>
2021-10-29 09:52:17 -0500

    adjustment response api

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,39 +2,45 @@
 import { IResolvers } from "apollo-server";
 import fetch from "node-fetch";
 
-import { MercadoLibreSearchResponse, RawProduct, Product, Description } from "./types";
+import { MercadoLibreSearchResponse, RawProduct, Description } from "./types";
 
 const url = 'https://api.mercadolibre.com/items/'
+const urlProducts = 'https://api.mercadolibre.com/sites/MLA/search?q='
 
 const resolvers: IResolvers = {
   Query: {
-    products: (_, args: { query: string }): Promise<Product[]> =>
-      fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${args.query}`)
-        .then((res) => res.json())
-        .then((response: MercadoLibreSearchResponse) =>
-          response.results.slice(0, 10).map((rawProduct) => ({
-            id: rawProduct.id,
-            title: rawProduct.title,
-            image: rawProduct.thumbnail,
-            price: rawProduct.price,
-            location: rawProduct.address.state_name,
-          })),
-        ),
+    products: async (_, args: { query: string }) => {
+      const products = await (await fetch(`${urlProducts}${args.query}`)).json();
+      return buildItems(products);
+    },
     product: async (_, args: { id: string }) => {
-      const products = await fetch(`${url}${args.id}`)
-      const description = await fetch(`${url}${args.id}/description`)
-      const resItems = await products.json();
-      const resDescription = await description.json();
-      const rawProduct: RawProduct = resItems;
-      rawProduct.id = resItems.id
-      rawProduct.price = resItems.price
-      rawProduct.title = resItems.title
-      rawProduct.image = resItems.thumbnail
-      rawProduct.location = resItems.seller_address.state.name
-      rawProduct.description = resDescription.plain_text
-      return rawProduct
+      const product = await (await fetch(`${url}${args.id}`)).json()
+      const description = await (await fetch(`${url}${args.id}/description`)).json()
+      return buildItem(product, description);
     }
   },
 };
+
+const buildItem = (resItem: RawProduct, resDescription: Description) => {
+  const rawProduct: RawProduct = resItem;
+  rawProduct.id = resItem.id
+  rawProduct.price = resItem.price
+  rawProduct.title = resItem.title
+  rawProduct.image = resItem.thumbnail
+  rawProduct.location = resItem.seller_address.state.name
+  rawProduct.description = resDescription.plain_text
+  return rawProduct;
+}
+
+const buildItems = (products: MercadoLibreSearchResponse) => {
+  const resultProducts = products.results.map((rawProduct) => ({
+    id: rawProduct.id,
+    title: rawProduct.title,
+    image: rawProduct.thumbnail,
+    price: rawProduct.price,
+    location: rawProduct.address.state_name,
+  }))
+  return resultProducts
+}
 
 export default resolvers;
